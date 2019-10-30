8efbfca9750eae11ef9f59a8c97e481fb2af5f6b
pattanachai_ac133 <pattanachai_ac133@yahoo.com>
2019-10-30 22:49:51 +0700

    fix webhook

--- server.js
+++ server.js
@@ -56,14 +56,6 @@ MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true },(e
         res.send('Hello');
     })
 
-    const convertMiddleWare = function(req,res,next){
-        req.agent = new WebhookClient({
-            request: req,
-            response: res
-        });
-        req.body = [{room: req.agent.parameters['hong']}]
-        next()
-    }
     const timearr = 
     ['08:00 - 08:30','08:30 - 09:00','09:00 - 09:30','09:30 - 10:00','10:00 - 10:30','10:30 - 11:00','11:00 - 11:30','11:30 - 12:00','12:00 - 12:30','12:30 - 13:00','13:00 - 13:30','13:30 - 14:00','14:00 - 14:30','14:30 - 15:00','15:00 - 15:30','15:30 - 16:00','16:00 - 16:30','16:30 - 17:00','17:00 - 17:30','17:30 - 18:00']
     const generateChatRes = function(arr){
@@ -73,7 +65,7 @@ MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true },(e
         }
         return text
     }
-    app.post('/webhook',convertMiddleWare,rservServices.checkRoom,(req,res)=>{
+    app.post('/webhook',(req,res)=>{
         
         const agent = new WebhookClient({request:req,response:res})
         
@@ -83,18 +75,32 @@ MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true },(e
             room = agent.context.get('hong_wang-custom-followup').parameters['hong.original']
             date = agent.context.get('hong_wang-custom-followup').parameters['date']
             date = date.substr(0,date.search('T'))
-            a = []
-            for(i=0;i<20;++i)a.push(false)
-            db.collection('reservations').find({room:room,date:date}).toArray((err,result)=>{
-                if(err) return res.status(500).send(err.toString())
-                for(i=0;i<result.length;++i)a[result[i].slot]=true;
-                agent.handleRequest((agent)=>{
-                    agent.add(generateChatRes(a))
-                })
+            db.collection('rooms').findOne({_id:room},(err,result)=>{
+                if(err){
+                    console.log(err.toString)
+                    return res.status(500)
+                }
+                if(!result){
+                    agent.handleRequest((agent)=>{
+                        agent.add('Unknown room: '+room)
+                    })
+                }
+                else{
+                    a = []
+                    for(i=0;i<20;++i)a.push(false)
+                    db.collection('reservations').find({room:room,date:date}).toArray((err,result)=>{
+                        if(err) return res.status(500).send(err.toString())
+                        for(i=0;i<result.length;++i)a[result[i].slot]=true;
+                        agent.handleRequest((agent)=>{
+                            agent.add(generateChatRes(a))
+                        })
+                    })
+                }
             })
+           
         }
-        else if(agent.intent=='hong_wang - custom - yes'){
-            req.agent.handleRequest((agent)=>{
+        else if(agent.intent=='hong_wang - custom'){
+                agent.handleRequest((agent)=>{
                 agent.context.set({'name':'hong_wang-custom-followup','lifespan':'10','parameters':agent.parameters})
                 agent.add('')
             })
